dbec65eadcc78c45b3c126d22a606a7eba45b347
esegebart <36378900+esegebart@users.noreply.github.com>
2020-06-29 11:59:56 -0500

    Finished another test for two

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -5,9 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest {
     @Test
     public void whenFizzBuzzIsPassedANumberItReturnsThatNumber() {
-        //simplest thing first
+        //simplest thing first, pass it a one
+        // Create new FizzBuzz object
+        // Expect 1, get a one
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("1", fizzBuzz.fizz(1));
+    }
+
+    @Test
+    public void whenFizzBuzzIsPassedATwoItReturnsATwo(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        // passing fizzbuzz a different number
+        assertEquals("2", fizzBuzz.fizz(2));
     }
 
 }
