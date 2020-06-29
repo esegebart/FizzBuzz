e3d8cce28df17c0c54eb2309f10954b45689f30a
esegebart <36378900+esegebart@users.noreply.github.com>
2020-06-29 13:20:54 -0500

    Finished FizzBuzz!

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,6 +1,10 @@
 public class FizzBuzz {
 
     public String fizz(Integer inputNumber) {
+        if((inputNumber % 3 == 0 && inputNumber % 5 == 0)){
+            return "fizzbuzz";
+        }
+
         if(inputNumber % 3 == 0)
             return "fizz";
         else if(inputNumber % 5 == 0)
