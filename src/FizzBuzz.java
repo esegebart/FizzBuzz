694cf6cbcd820df17b82124215a4b848487b523e
esegebart <36378900+esegebart@users.noreply.github.com>
2020-06-29 12:56:29 -0500

    Refactored FizzBuzz objects to BeforeEach setUp method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,9 +1,8 @@
 public class FizzBuzz {
 
     public String fizz(Integer inputNumber) {
-        if (inputNumber % 3 == 0){
+        if (inputNumber % 3 == 0)
             return "fizz";
-        }
         // now returning numbers to string
         return inputNumber.toString();
     }
--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -1,27 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+    FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    public void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void whenFizzBuzzIsPassedANumberItReturnsThatNumber() {
-        //simplest thing first, pass it a one
-        // Create new FizzBuzz object
-        // Expect 1, get a one
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("1", fizzBuzz.fizz(1));
     }
 
     @Test
     public void whenFizzBuzzIsPassedATwoItReturnsATwo(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
         // passing fizzbuzz a different number
         assertEquals("2", fizzBuzz.fizz(2));
     }
 
     @Test
     public void whenFizzBuzzIsPassedAMultipleOfThreeItReturnsFizz(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("fizz", fizzBuzz.fizz(3));
         assertEquals("fizz", fizzBuzz.fizz(6));
     }
